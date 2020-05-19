caf042e4262a116b62da99c5bb4328fb54842faa
harshay <harshaykr@hotmail.com>
2020-05-19 21:14:24 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-/*test*/
+/******************************************************************************************************** 
+ * Name : Harshay
+ * Teamtreehouse Techdegree
+ * Project 5 : Random User Generator API  
+ * Full Stack Javascript
+ * Grading Level : Exceeds
+********************************************************************************************************/
+
+//API Documentation : https://randomuser.me/documentation
+//url for extracting data using the fetch api : 'https://randomuser.me/api/'      
+
+/*
+Get and display 12 random users
+With information provided from The Random User Generator API, send a single request to the API, and use the response data to display 12 users, along with some basic information for each:
+Image
+First and Last Name
+Email
+City or location
+Refer to the mockups and the comments in the index.html file for an example of what info should be displayed on the page and how it should be styled.
+*/
