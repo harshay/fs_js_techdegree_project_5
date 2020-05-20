392ee9fa1eb1d28d6cd342c98e69a719efb7c9ce
harshay <harshaykr@hotmail.com>
2020-05-20 22:12:19 +0100

    update

--- js/script.js
+++ js/script.js
@@ -17,4 +17,50 @@ First and Last Name
 Email
 City or location
 Refer to the mockups and the comments in the index.html file for an example of what info should be displayed on the page and how it should be styled.
+*/   
+
+/********************************************************************************************************/
+
+
+
+
+/********************************************************************************************************/
+/*
+use fetch api 
+fetch functions
+*/
+
+fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => console.log(data.results[0]));
+
+    
+/********************************************************************************************************/
+
+/*
+
+
+                <div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">first last</h3>
+                        <p class="card-text">email</p>
+                        <p class="card-text cap">city, state</p>
+                    </div>
+                </div>
+
+*/
+
+/*
+
+1 - create  a div with class card 
+2 - create another div (1) within the div created in step 1 : class="card-img-container"
+3 - create another div (2) within the div created in step 1 : class="card-info-container"  
+4 - create a string containing the img tag and append to div created in step 2 ; 
+5 - create a string containing a h3 tag and append to div created in step 3 ; 
+6 - create a string containing a p (1) tag and append to div created in step 3 ; 
+7 - create a string containing a p (2) tag and append to div created in step 3 ; 
+
 */
