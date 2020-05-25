bc4628f5d4da32e7c24a181337f4b9155d9d59d7
harshay <harshaykr@hotmail.com>
2020-05-25 19:50:48 +0100

    update

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ let glry_div = document.getElementById('gallery');
 let all_employees = [];
 let all_employee_names = [];
 
+
 //fetch ; store all results 
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
@@ -43,7 +44,11 @@ let api_result = (empl_obj) =>  {
 
     };
 
+    let all_cards = document.getElementsByClassName('card');
+
 };
+
+
 
 /////////////////////////////////////////////////////////
 
@@ -177,7 +182,7 @@ let search_func = () => {
 }; 
 
 //reset search results when visitor clicks outside the search box
-document.body.addEventListener('click', (event) =>{
+document.body.addEventListener('click', (event) => {
  
     //all employee cards
     let all_employee_cards = document.getElementsByClassName('card');   
@@ -212,5 +217,150 @@ form_input_1_html.addEventListener('keyup', (event) => {
 });
 
 /********************************************************************************************************/
-
 //modal window 
+
+
+console.log(all_cards);
+
+
+
+
+let mod_window_func = (empl_mod_obj) => { 
+
+    let mod_container = document.createElement('div'); 
+    mod_container.className = 'modal-container'; 
+
+    ////////////////////////////////////////////////////////
+
+    let mod_main_div = document.createElement('div'); 
+    mod_main_div.className = 'modal'; 
+
+    mod_container.appendChild(mod_main_div);
+
+    ////////////////////////////////////////////////////////
+
+    //button
+    let mod_button = document.createElement('button'); 
+    let mod_button_strong = document.createElement('strong'); 
+
+    mod_button.type = 'button';
+    mod_button.id = 'modal-close-btn'; 
+    mod_button.className = 'modal-close-btn';
+
+
+    mod_main_div.appendChild(mod_button);
+    mod_button.appendChild(mod_button_strong);
+
+    mod_button_strong.innerHTML = 'X';
+
+    ////////////////////////////////////////////////////////
+
+    //info container
+    let mod_info_container = document.createElement('div'); 
+    mod_info_container.className = 'modal-info-container';
+
+    mod_main_div.appendChild(mod_info_container);
+
+    ////////////////////////////////////////////////////////
+
+    //modal window img tag 
+    let mod_img_tag = doument.createElement('img'); 
+    mod_img_tag.src = empl_mod_obj.picture.medium;
+    mod_img_tag.className = 'modal-img'; 
+    mod_img_tag.alt = 'profile picture';  
+
+    mod_info_container.appendChild(mod_img_tag); 
+
+    ////////////////////////////////////////////////////////
+
+    //h3 tag
+    //header 
+    let mod_h3_tag = document.createElement('h3');
+    mod_h3_tag.className = 'modal-name cap';
+    mod_h3_tag.id = 'name';
+
+    mod_h3_tag.innerHTML = empl_mod_obj.name.first + ' ' + empl_mod_obj.name.last;
+    
+    mod_info_container.appendChild(mod_h3_tag); 
+
+    ////////////////////////////////////////////////////////
+    
+    //email and city 
+    let mod_p_email = document.createElement('p');
+    mod_p_email.className = 'modal-text';
+
+    mod_p_email.innerHTML =  empl_mod_obj.email; 
+
+    ///////
+
+    let mod_p_city = document.createElement('p');
+    mod_p_city.className = 'modal-text cap';
+
+    mod_p_city.innerHTML =  empl_mod_obj.location.city  + ', ' + empl_mod_obj.location.state;
+
+    mod_info_container.appendChild(mod_p_email);
+    mod_info_container.appendChild(mod_p_city);
+
+    ////////////////////////////////////////////////////////
+
+    let mod_hr = document.createElement('hr'); 
+
+    mod_info_container.appendChild(mod_hr);
+
+    ////////////////////////////////////////////////////////
+
+    //phone, street, bday 
+    let mod_p_phone = document.createElement('p');
+    let mod_p_street = document.createElement('p');
+    let mod_p_bday = document.createElement('p');
+
+    mod_p_phone.className = 'modal-text';
+    mod_p_street.className = 'modal-text';
+    mod_p_bday.className = 'modal-text';
+
+    mod_p_phone.innerHTML = empl_mod_obj.phone;
+    mod_p_street.innerHTML = empl_mod_obj.location.street;
+    mod_p_bday.innerHTML = empl_mod_obj.dob.date;
+
+    mod_info_container.appendChild(mod_p_phone);
+    mod_info_container.appendChild(mod_p_street);
+    mod_info_container.appendChild(mod_p_bday);
+
+    ////////////////////////////////////////////////////////
+
+    let mod_btn_container = document.createElement('div'); 
+    mod_btn_container.className = 'modal-btn-container';      
+
+    mod_container.appendChild(mod_btn_container); 
+
+    //////////////////////////////////////////////////////// 
+
+    let mod_btn_container_b1 = document.createElement('button'); 
+    let mod_btn_container_b2 = document.createElement('button'); 
+
+    mod_btn_container_b1.type = 'button';
+    mod_btn_container_b2.type = 'button'; 
+    
+    mod_btn_container_b1.id = 'modal-prev';
+    mod_btn_container_b2.id = 'modal-next'; 
+    
+    mod_btn_container_b1.className = 'modal-prev btn';
+    mod_btn_container_b2.ClassName = 'modal-next btn'; 
+    
+    mod_btn_container_b1.innerHTML = 'Prev';
+    mod_btn_container_b2.innerHTML = 'Next';
+
+    mod_btn_container.appendChild(mod_btn_container_b1);
+    mod_btn_container.appendChild(mod_btn_container_b2);
+
+    //////////////////////////////////////////////////////// 
+    
+}; 
+
+
+
+
+
+
+
+/********************************************************************************************************/
